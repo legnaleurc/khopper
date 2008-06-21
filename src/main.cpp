4a12dcd476d28e06d810d8eb67199e71bb5369ea
legnaleurc <legnaleurc@gmail.com>
2008-06-21 11:39:15 +0800

    Clear test case.

--- src/main.cpp
+++ src/main.cpp
@@ -2,45 +2,12 @@
 
 #include "ui/MainWindow.hpp"
 
-#include <vector>
-#include <map>
-
-#include <cstdlib>
-#include "unistd.h"
-	
-const char * const LIST = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-std::string strFromRand() {
-	std::string result;
-	int len = rand() % 52;
-	result.reserve( len );
-	for( int i = 0; i < len; ++i ) {
-		result.push_back( LIST[rand()%52] );
-	}
-	return result;
-}
-
 int main( int argc, char * argv[] ) {
 	QApplication app( argc, argv );
 	
 	Khopper::MainWindow window;
 	window.resize( 640, 480 );
 	window.show();
-	
-	srand( getpid() );
-	
-	std::vector< Khopper::CUESheet::FieldType > songs;
-	int len = rand() % 9 + 1;
-	songs.reserve( len );
-	for( int i = 0; i < len; ++i ) {
-		Khopper::CUESheet::FieldType temp;
-		for( int j = 0; j < Khopper::CUESheet::Track::SIZE; ++j ) {
-			temp.push_back( strFromRand() );
-		}
-		songs.push_back( temp );
-	}
-	
-	window.setSongList( songs );
 	
 	return app.exec();
 }
