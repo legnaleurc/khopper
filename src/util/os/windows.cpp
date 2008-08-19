991443a4f2cb5a8e23d8a0cf59c21ad0effc25ea
legnaleurc <legnaleurc@gmail.com>
2008-08-19 13:26:58 +0800

    Add windows file.

---
+++ src/util/os/windows.cpp
@@ -0,0 +1,14 @@
+#include "os.hpp"
+
+#include <windows.h>
+
+namespace Khopper {
+
+	int os::getResult( std::pair<std::string,std::string> & msg, const std::vector<std::string> & args ) {
+		return 0;
+	}
+
+	void os::exists( const std::string & exe ) {
+	}
+
+}
