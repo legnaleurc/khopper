e8ce18725b7d17657eb0cc20c3c0c94682b7f990
legnaleurc <legnaleurc@gmail.com>
2008-08-21 15:09:49 +0800

    Fix MSVC project file.

--- src/util/windows.cpp
+++ src/util/windows.cpp
@@ -1,14 +1,22 @@
-#include "os.hpp"
+#include "util/os.hpp"
 
 #include <windows.h>
 
 namespace Khopper {
 
-	int os::getResult( std::pair<std::string,std::string> & msg, const std::vector<std::string> & args ) {
-		return 0;
-	}
+	namespace os {
 
-	void os::exists( const std::string & exe ) {
+		int getResult( std::pair<std::string,std::string> & msg, const std::vector<std::string> & args ) {
+			return 0;
+		}
+
+		int getResult( std::pair<std::string,std::string> & msg, const std::vector<std::string> & args, const std::string & input ) {
+			return 0;
+		}
+
+		void exists( const std::string & exe ) {
+		}
+
 	}
 
 }
