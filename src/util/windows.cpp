e2c501fc3872fc8f5fddb26d2f41e2d11bcdbfe5
legnaleurc <legnaleurc@gmail.com>
2008-08-26 22:21:36 +0800

    Windows dummy implementation.

--- src/util/windows.cpp
+++ src/util/windows.cpp
@@ -6,15 +6,15 @@ namespace Khopper {
 
 	namespace os {
 
-		int getResult( std::pair<std::string,std::string> & msg, const std::vector<std::string> & args ) {
-			return 0;
-		}
-
 		int getResult( std::pair<std::string,std::string> & msg, const std::vector<std::string> & args, const std::string & input ) {
 			return 0;
 		}
 
 		void exists( const std::string & exe ) {
+		}
+		
+		std::string join( const std::string & front, const std::string & back ) {
+			return back;
 		}
 
 	}
