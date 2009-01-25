2f73f2e6281d8e61a4a3d85fa9c2aaeec7e828f0
legnaleurc <legnaleurc@gmail.com>
2009-01-26 04:02:38 +0800

    [BugFix] Initialize avcodec.

--- src/khopper.cpp
+++ src/khopper.cpp
@@ -1,9 +1,14 @@
+#include "mainwindow.hpp"
+
 #include <QApplication>
 
-#include "mainwindow.hpp"
+extern "C" {
+#include <avformat.h>
+}
 
 int main( int argc, char * argv[] ) {
 	QApplication app( argc, argv );
+	av_register_all();
 	
 	Khopper::MainWindow window;
 	window.resize( 640, 480 );
