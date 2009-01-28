a54163133bd092ce40602b0efda6992e94fe6c55
legnaleurc <legnaleurc@gmail.com>
2009-01-29 00:07:18 +0800

    [BugFix] Missing file.

---
+++ src/ui/abstractoption.cpp
@@ -0,0 +1,11 @@
+#include "abstractoption.hpp"
+
+namespace Khopper {
+
+	AbstractOption::AbstractOption( QWidget * parent, Qt::WindowFlags f ) : QWidget( parent, f ) {
+	}
+
+	AbstractOption::~AbstractOption() {
+	}
+
+}
