d272b26525d5290c831b896d7bb0f357e19af2d1
legnaleurc <legnaleurc@gmail.com>
2009-02-04 00:21:24 +0800

    [Doc] Update license documentation.

--- src/widgets/mainwindow.cpp
+++ src/widgets/mainwindow.cpp
@@ -1,3 +1,24 @@
+/**
+ * @file mainwindow.cpp
+ * @author Wei-Cheng Pan
+ *
+ * Copyright (C) 2008 Wei-Cheng Pan <legnaleurc@gmail.com>
+ *
+ * This file is part of Khopper.
+ *
+ * Khopper is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Khopper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "mainwindow.hpp"
 #include "songlist.hpp"
 #include "threads.hpp"
