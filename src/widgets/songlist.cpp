82ec085cb25d36ddc0cf2220da9f0c151da57e02
legnaleurc <legnaleurc@gmail.com>
2009-02-07 02:05:17 +0800

    [Source] Remove no needed code.

--- src/widgets/songlist.cpp
+++ src/widgets/songlist.cpp
@@ -20,7 +20,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "songlist.hpp"
-#include "track.hpp"
 
 #include <QDragEnterEvent>
 #include <QDragMoveEvent>
