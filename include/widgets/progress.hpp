1f0000f1eee4c78d4b7811299b6cd7b23463f494
legnaleurc <legnaleurc@gmail.com>
2009-02-08 02:01:07 +0800

    [BugFix] Add missing files.

---
+++ include/widgets/progress.hpp
@@ -0,0 +1,81 @@
+/**
+ * @file progress.hpp
+ * @author Wei-Cheng Pan
+ *
+ * Copyright (C) 2008 Wei-Cheng Pan <legnaleurc@gmail.com>
+ *
+ * This file is part of Khopper.
+ *
+ * Khopper is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Khopper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef KHOPPER_PROGRESS_HPP
+#define KHOPPER_PROGRESS_HPP
+
+#include <QDialog>
+#include <QLabel>
+#include <QProgressBar>
+
+namespace Khopper {
+
+	/**
+	 * @brief Show the progress
+	 */
+	class Progress : public QDialog {
+		Q_OBJECT
+
+	public:
+		/**
+		 * @brief Pass arguments to QDialog
+		 */
+		Progress( QWidget * parent = 0, Qt::WindowFlags f = 0 );
+
+		/**
+		 * @brief Get current progress
+		 * @sa setValue
+		 */
+		int getValue() const;
+
+	public slots:
+		/**
+		 * @brief Set maximum progress
+		 */
+		void setMaximum( int maximum );
+		/**
+		 * @brief Set current progress
+		 * @sa getValue
+		 */
+		void setValue( int value );
+		/**
+		 * @brief Set current item name
+		 */
+		void setItemName( const QString & name );
+		/**
+		 * @brief Set current task count
+		 */
+		void setCurrent( int current );
+		/**
+		 * @brief Set total tasks
+		 */
+		void setTotal( int total );
+
+	private:
+		QLabel * itemName_;
+		QLabel * current_;
+		QLabel * total_;
+		QProgressBar * prog_;
+	};
+
+}
+
+#endif
---
+++ src/widgets/progress.cpp
@@ -0,0 +1,85 @@
+/**
+ * @file progress.cpp
+ * @author Wei-Cheng Pan
+ *
+ * Copyright (C) 2008 Wei-Cheng Pan <legnaleurc@gmail.com>
+ *
+ * This file is part of Khopper.
+ *
+ * Khopper is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Khopper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "progress.hpp"
+
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QPushButton>
+
+namespace Khopper {
+
+	Progress::Progress( QWidget * parent, Qt::WindowFlags f ):
+	QDialog( parent, f ),
+	itemName_( new QLabel( this ) ),
+	current_( new QLabel( this ) ),
+	total_( new QLabel( this ) ),
+	prog_( new QProgressBar( this ) ) {
+		this->setWindowTitle( tr( "Converting ..." ) );
+
+		QVBoxLayout * mainBox = new QVBoxLayout( this );
+		this->setLayout( mainBox );
+		mainBox->setAlignment( Qt::AlignCenter );
+
+		mainBox->addWidget( this->itemName_ );
+
+		QHBoxLayout * midBox = new QHBoxLayout;
+		mainBox->addLayout( midBox );
+		midBox->setAlignment( Qt::AlignCenter );
+
+		midBox->addWidget( this->current_ );
+		midBox->addWidget( new QLabel( "/", this ) );
+		midBox->addWidget( this->total_ );
+
+		mainBox->addWidget( this->prog_ );
+		this->prog_->setRange( 0, 0 );
+
+		QPushButton * cancel = new QPushButton( tr( "&Cancel" ), this );
+		mainBox->addWidget( cancel );
+		connect( cancel, SIGNAL( clicked() ), this, SLOT( reject() ) );
+	}
+
+	int Progress::getValue() const {
+		return this->prog_->value();
+	}
+
+	void Progress::setMaximum( int maximum ) {
+		this->prog_->setMaximum( maximum );
+		this->prog_->setValue( 0 );
+	}
+
+	void Progress::setValue( int value ) {
+		this->prog_->setValue( value );
+	}
+
+	void Progress::setItemName( const QString & name ) {
+		this->itemName_->setText( name );
+	}
+
+	void Progress::setCurrent( int current ) {
+		this->current_->setNum( current );
+	}
+
+	void Progress::setTotal( int total ) {
+		this->total_->setNum( total );
+	}
+
+}
