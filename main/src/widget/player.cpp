4101997082ce71989688edcbe71da7c62b15be25
legnaleurc <legnaleurc@gmail.com>
2009-11-01 20:58:09 +0800

    [BugFix] Fix wrong connection.

--- main/src/widget/player.cpp
+++ main/src/widget/player.cpp
@@ -66,7 +66,7 @@ namespace khopper {
 			playerBox->addWidget( this->seeker_ );
 			playerBox->addWidget( this->volume_ );
 
-			connect( this->songList_, SIGNAL( dropFile( const QStringList & ) ), this, SIGNAL( dropFile( const QStringList & ) ) );
+			connect( this->songList_, SIGNAL( fileDropped( const QStringList & ) ), this, SIGNAL( fileDropped( const QStringList & ) ) );
 			connect( this->songList_, SIGNAL( requireConvert() ), this, SIGNAL( requireConvert() ) );
 			mainBox->addWidget( this->songList_ );
 		}
