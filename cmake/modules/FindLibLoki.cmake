538baff33d3ef8536a6433121b91a5e2e2ad05ff
legnaleurc <legnaleurc@gmail.com>
2009-12-06 06:13:44 +0800

    [EXPREMENTAL] Fix some module.

--- cmake/modules/FindLibLoki.cmake
+++ cmake/modules/FindLibLoki.cmake
@@ -3,13 +3,14 @@
 #
 # Below is a detailed list of variables that FindLibLoki.cmake sets.
 # LIBLOKI_FOUND       If false, don't try to use libloki
+#
 # LIBLOKI_INCLUDE_DIR Path to "include" of libloki
 # LIBLOKI_LIBRARIES   The libloki libraries
 
 find_path(LIBLOKI_INCLUDE_DIR NAMES loki/Factory.h loki/Singleton.h)
-find_library(LIBLOKI_LIBRARIES NAMES loki)
+find_library(LIBLOKI_LIBRARY NAMES loki)
 
-if(LIBLOKI_INCLUDE_DIR AND LIBLOKI_LIBRARIES)
+if(LIBLOKI_INCLUDE_DIR AND LIBLOKI_LIBRARY)
     message(STATUS "Found libloki")
     set(LIBLOKI_FOUND TRUE)
 else()
