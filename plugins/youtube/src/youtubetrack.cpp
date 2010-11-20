c0d0c2170ed1b6eaf32d647a3d7838551d5d3b62
legnaleurc <legnaleurc@gmail.com>
2010-11-20 22:59:44 +0800

    [EXPREMENTAL] Complete remain thing.

--- plugins/youtube/src/youtubetrack.cpp
+++ plugins/youtube/src/youtubetrack.cpp
@@ -1,8 +1,20 @@
 #include "youtubetrack.hpp"
+#include "youtubeloader.hpp"
+#include "youtubereader.hpp"
+
+#include <QtCore/QEventLoop>
 
 using namespace khopper::album;
+using khopper::plugin::YouTubeLoader;
+using khopper::plugin::VideoParameter;
+using khopper::codec::ReaderSP;
+using khopper::codec::YouTubeReader;
 
 YouTubeTrack::YouTubeTrack( const QUrl & uri, const QString & format ):
 Track( uri ),
 format_( format ) {
+}
+
+ReaderSP YouTubeTrack::createReader() const {
+	return ReaderSP( new YouTubeReader( YouTubeLoader::getRealUri( this->getURI(), this->format_ ) ) );
 }
