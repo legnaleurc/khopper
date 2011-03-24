7934f117bfb52b30d3b4e0f5fae998b0f630b61f
legnaleurc <legnaleurc@gmail.com>
2011-03-24 17:15:02 +0800

    [Source] Fix build script.

--- cmake/modules/FindFFmpeg.cmake
+++ cmake/modules/FindFFmpeg.cmake
@@ -7,51 +7,41 @@
 # FFMPEG_INCLUDE_DIR   Include directory necessary for using the FFmpeg headers
 # FFMPEG_LIBRARIES     Link this to use FFmpeg
 
-if(FFMPEG_INCLUDE_DIR AND FFMPEG_LIBRARIES)
+if(NOT FFMPEG_FOUND)
     set(FFMPEG_FOUND TRUE)
 else()
-    if(NOT WIN32)
-        find_package(PkgConfig)
+    find_package(PkgConfig)
+    if(PKG_CONFIG_FOUND)
         pkg_check_modules(PC_FFMPEG libavformat libavcodec libavutil)
-        set(FFMPEG_DEFINITIONS
-            ${PC_FFMPEG_libavformat_CFLAGS_OTHER}
-            ${PC_FFMPEG_libavcodec_CFLAGS_OTHER}
-            ${PC_FFMPEG_libavutil_CFLAGS_OTHER})
-    endif()
+        if(PC_FFMPEG_FOUND)
+            set(FFMPEG_DEFINITIONS ${PC_FFMPEG_CFLAGS_OTHER})
+            set(FFMPEG_INCLUDE_DIR ${PC_FFMPEG_INCLUDE_DIRS} CACHE INTERNAL "")
+            set(FFMPEG_LIBRARIES ${PC_FFMPEG_LIBRARIES} CACHE INTERNAL "")
+            set(FFMPEG_FOUND TRUE CACHE INTERNAL "")
+        endif()
+    else()
+        find_path(FFMPEG_INCLUDE_DIR
+            NAMES libavformat/avformat.h libavcodec/avcodec.h)
+        find_library(FFMPEG_FORMAT_LIBRARY avformat)
+        find_library(FFMPEG_CODEC_LIBRARY avcodec)
+        find_library(FFMPEG_UTIL_LIBRARY avutil)
 
-    find_path(FFMPEG_INCLUDE_DIR
-        NAMES libavformat/avformat.h libavcodec/avcodec.h
-        HINTS
-            ${PC_FFMPEG_libavformat_INCLUDEDIR}
-            ${PC_FFMPEG_libavformat_INCLUDE_DIRS}
-            ${PC_FFMPEG_libavcodec_INCLUDEDIR}
-            ${PC_FFMPEG_libavcodec_INCLUDE_DIRS})
-    find_library(FFMPEG_FORMAT_LIBRARY avformat
-        HINTS
-            ${PC_FFMPEG_libavformat_LIBDIR}
-            ${PC_FFMPEG_libavformat_LIBRARY_DIRS})
-    find_library(FFMPEG_CODEC_LIBRARY avcodec
-        HINTS
-            ${PC_FFMPEG_libavcodec_LIBDIR}
-            ${PC_FFMPEG_libavcodec_LIBRARY_DIRS})
-    find_library(FFMPEG_UTIL_LIBRARY avutil
-        HINTS
-            ${PC_FFMPEG_libavutil_LIBDIR}
-            ${PC_FFMPEG_libavutil_LIBRARY_DIRS})
+        set(FFMPEG_LIBRARIES)
+        if(FFMPEG_FORMAT_LIBRARY)
+            set(FFMPEG_LIBRARIES ${FFMPEG_LIBRARIES} ${FFMPEG_FORMAT_LIBRARY})
+        endif()
+        if(FFMPEG_CODEC_LIBRARY)
+            set(FFMPEG_LIBRARIES ${FFMPEG_LIBRARIES} ${FFMPEG_CODEC_LIBRARY})
+        endif()
+        if(FFMPEG_UTIL_LIBRARY)
+            set(FFMPEG_LIBRARIES ${FFMPEG_LIBRARIES} ${FFMPEG_UTIL_LIBRARY})
+        endif()
 
-    set(FFMPEG_LIBRARIES)
-    if(FFMPEG_FORMAT_LIBRARY)
-        set(FFMPEG_LIBRARIES ${FFMPEG_LIBRARIES} ${FFMPEG_FORMAT_LIBRARY})
-    endif()
-    if(FFMPEG_CODEC_LIBRARY)
-        set(FFMPEG_LIBRARIES ${FFMPEG_LIBRARIES} ${FFMPEG_CODEC_LIBRARY})
-    endif()
-    if(FFMPEG_UTIL_LIBRARY)
-        set(FFMPEG_LIBRARIES ${FFMPEG_LIBRARIES} ${FFMPEG_UTIL_LIBRARY})
-    endif()
+        mark_as_advanced(FFMPEG_FORMAT_LIBRARY FFMPEG_CODEC_LIBRARY FFMPEG_UTIL_LIBRARY)
 
-    if(FFMPEG_INCLUDE_DIR AND FFMPEG_LIBRARIES)
-        set(FFMPEG_FOUND TRUE)
+        if(FFMPEG_INCLUDE_DIR AND FFMPEG_LIBRARIES)
+            set(FFMPEG_FOUND TRUE)
+        endif()
     endif()
 
     if(FFMPEG_FOUND)
@@ -68,6 +58,4 @@ else()
             endif()
         endif()
     endif()
-
-    mark_as_advanced(FFMPEG_FORMAT_LIBRARY FFMPEG_CODEC_LIBRARY FFMPEG_UTIL_LIBRARY)
 endif()
