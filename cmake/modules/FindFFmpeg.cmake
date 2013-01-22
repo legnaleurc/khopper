c4121ac224f53228c4d62c1348bda93e2b0c3ce4
Wei Cheng Pan <legnaleurc@gmail.com>
2013-01-22 14:41:39 +0800

    Update CMake package finding script.

--- cmake/modules/FindFFmpeg.cmake
+++ cmake/modules/FindFFmpeg.cmake
@@ -1,11 +1,13 @@
-# - Find FFmpeg
-# This module can be used to find FFmpeg
+# - Find FFMPEG
+# This module can be used to find FFMPEG
 # Once done this will define
 #
-# FFMPEG_FOUND         If false, don't try to use FFmpeg
+# FFMPEG_FOUND         If false, don't try to use FFMPEG
 #
-# FFMPEG_INCLUDE_DIR   Include directory necessary for using the FFmpeg headers
-# FFMPEG_LIBRARIES     Link this to use FFmpeg
+# FFMPEG_INCLUDE_DIRS  Include directory necessary for using the FFMPEG headers
+# FFMPEG_LIBRARIES     Link this to use FFMPEG
+
+include(KhopperUtility)
 
 if(NOT FFMPEG_FOUND)
     find_package(PkgConfig)
@@ -13,47 +15,41 @@ if(NOT FFMPEG_FOUND)
         pkg_check_modules(PC_FFMPEG libavformat libavcodec libavutil)
         if(PC_FFMPEG_FOUND)
             set(FFMPEG_DEFINITIONS ${PC_FFMPEG_CFLAGS_OTHER})
-            set(FFMPEG_INCLUDE_DIR ${PC_FFMPEG_INCLUDE_DIRS} CACHE INTERNAL "")
-            set(FFMPEG_LIBRARIES ${PC_FFMPEG_LIBRARIES} CACHE INTERNAL "")
-            set(FFMPEG_FOUND TRUE CACHE INTERNAL "")
+            set(FFMPEG_INCLUDE_DIRS ${PC_FFMPEG_INCLUDE_DIRS})
+            set(FFMPEG_LIBRARIES ${PC_FFMPEG_LIBRARIES})
         endif()
     else()
-        find_path(FFMPEG_INCLUDE_DIR
-            NAMES libavformat/avformat.h libavcodec/avcodec.h)
-        find_library(FFMPEG_FORMAT_LIBRARY avformat)
-        find_library(FFMPEG_CODEC_LIBRARY avcodec)
-        find_library(FFMPEG_UTIL_LIBRARY avutil)
+        path_from_env(FFMPEG_ROOT)
+
+        find_path(FFMPEG_INCLUDE_DIRS
+            NAMES libavformat/avformat.h libavcodec/avcodec.h
+            HINTS ${FFMPEG_ROOT}
+            PATH_SUFFIXES "include")
+        find_library(FFMPEG_FORMAT_LIBRARY avformat
+            HINTS ${FFMPEG_ROOT}
+            PATH_SUFFIXES "lib")
+        find_library(FFMPEG_CODEC_LIBRARY avcodec
+            HINTS ${FFMPEG_ROOT}
+            PATH_SUFFIXES "lib")
+        find_library(FFMPEG_UTIL_LIBRARY avutil
+            HINTS ${FFMPEG_ROOT}
+            PATH_SUFFIXES "lib")
 
         set(FFMPEG_LIBRARIES)
         if(FFMPEG_FORMAT_LIBRARY)
-            set(FFMPEG_LIBRARIES ${FFMPEG_LIBRARIES} ${FFMPEG_FORMAT_LIBRARY})
+            list(APPEND FFMPEG_LIBRARIES ${FFMPEG_FORMAT_LIBRARY})
         endif()
         if(FFMPEG_CODEC_LIBRARY)
-            set(FFMPEG_LIBRARIES ${FFMPEG_LIBRARIES} ${FFMPEG_CODEC_LIBRARY})
+            list(APPEND FFMPEG_LIBRARIES ${FFMPEG_CODEC_LIBRARY})
         endif()
         if(FFMPEG_UTIL_LIBRARY)
-            set(FFMPEG_LIBRARIES ${FFMPEG_LIBRARIES} ${FFMPEG_UTIL_LIBRARY})
-        endif()
-
-        mark_as_advanced(FFMPEG_FORMAT_LIBRARY FFMPEG_CODEC_LIBRARY FFMPEG_UTIL_LIBRARY)
-
-        if(FFMPEG_INCLUDE_DIR AND FFMPEG_LIBRARIES)
-            set(FFMPEG_FOUND TRUE)
+            list(APPEND FFMPEG_LIBRARIES ${FFMPEG_UTIL_LIBRARY})
         endif()
     endif()
 
-    if(FFMPEG_FOUND)
-        set(FFMPEG_LIBRARIES ${FFMPEG_LIBRARIES} CACHE STRING "FFmpeg libraries" FORCE)
-        if(NOT FFmpeg_FIND_QUIETLY)
-            message(STATUS "Found FFmpeg: ${FFMPEG_LIBRARIES} ${FFMPEG_INCLUDE_DIR}")
-        endif()
-    else()
-        if(FFmpeg_FIND_REQUIRED)
-            message(FATAL_ERROR "Could NOT find FFmpeg")
-        else()
-            if(NOT FFmpeg_FIND_QUIETLY)
-                message(STATUS "Could NOT find FFmpeg")
-            endif()
-        endif()
-    endif()
+    include(FindPackageHandleStandardArgs)
+
+    find_package_handle_standard_args(FFMPEG DEFAULT_MSG FFMPEG_INCLUDE_DIRS FFMPEG_LIBRARIES)
+
+    mark_as_advanced(FFMPEG_INCLUDE_DIRS FFMPEG_LIBRARIES)
 endif()
--- cmake/modules/FindTagLib.cmake
+++ cmake/modules/FindTagLib.cmake
@@ -2,48 +2,45 @@
 # This module can be used to find taglib
 # Once done this will define
 #
-# TAGLIB_FOUND       If false, don't try to use taglib
+# TAGLIB_FOUND        If false, don't try to use taglib
 #
-# TAGLIB_INCLUDE_DIR The taglib include directory
-# TAGLIB_LIBRARIES   Link these to use taglib
+# TAGLIB_INCLUDE_DIRS The taglib include directory
+# TAGLIB_LIBRARIES    Link these to use taglib
+
+include(KhopperUtility)
 
 if(NOT TAGLIB_FOUND)
     find_package(PkgConfig)
     if(PKG_CONFIG_FOUND)
         pkg_check_modules(PC_TAGLIB taglib)
         if(PC_TAGLIB_FOUND)
-            set(TAGLIB_INCLUDE_DIR ${PC_TAGLIB_INCLUDE_DIRS} CACHE INTERNAL "")
-            set(TAGLIB_LIBRARIES ${PC_TAGLIB_LIBRARIES} CACHE INTERNAL "")
-            set(TAGLIB_FOUND TRUE CACHE INTERNAL "")
+            set(TAGLIB_INCLUDE_DIRS ${PC_TAGLIB_INCLUDE_DIRS})
+            set(TAGLIB_LIBRARIES ${PC_TAGLIB_LIBRARIES})
         endif()
     else()
-        find_path(TAGLIB_INCLUDE_DIR id3v2tag.h)
-        find_library(TAGLIB_LIBRARIES_RELEASE tag)
-        find_library(TAGLIB_LIBRARIES_DEBUG tagd)
+        path_from_env(TAGLIB_ROOT)
+
+        find_path(TAGLIB_INCLUDE_DIRS id3v2tag.h
+            HINTS ${TAGLIB_ROOT}
+            PATH_SUFFIXES "include/taglib")
+        find_library(TAGLIB_LIBRARIES_RELEASE tag
+            HINTS ${TAGLIB_ROOT}
+            PATH_SUFFIXES "lib")
+        find_library(TAGLIB_LIBRARIES_DEBUG tagd
+            HINTS ${TAGLIB_ROOT}
+            PATH_SUFFIXES "lib")
 
         if(TAGLIB_LIBRARIES_RELEASE AND TAGLIB_LIBRARIES_DEBUG)
             set(TAGLIB_LIBRARIES optimized ${TAGLIB_LIBRARIES_RELEASE} debug ${TAGLIB_LIBRARIES_DEBUG})
         elseif(TAGLIB_LIBRARIES_RELEASE)
             set(TAGLIB_LIBRARIES ${TAGLIB_LIBRARIES_RELEASE})
         endif()
-
-        if(TAGLIB_INCLUDE_DIR AND TAGLIB_LIBRARIES)
-            set(TAGLIB_FOUND TRUE)
-        endif()
     endif()
 
-    if(TAGLIB_FOUND)
-        set(TAGLIB_LIBRARIES ${TAGLIB_LIBRARIES} CACHE STRING "TabLib libraries" FORCE)
-        if(NOT TagLib_FIND_QUIETLY)
-            message(STATUS "Found TagLib: ${TAGLIB_LIBRARIES} ${TAGLIB_INCLUDE_DIR}")
-        endif()
-    else()
-        if(TagLib_FIND_REQUIRED)
-            message(FATAL_ERROR "Could NOT find TagLib")
-        else()
-            if(NOT TagLib_FIND_QUIETLY)
-                message(STATUS "Could NOT find TagLib")
-            endif()
-        endif()
-    endif()
+    include(FindPackageHandleStandardArgs)
+
+    find_package_handle_standard_args(TAGLIB DEFAULT_MSG TAGLIB_INCLUDE_DIRS TAGLIB_LIBRARIES)
+
+    mark_as_advanced(TAGLIB_INCLUDE_DIRS TAGLIB_LIBRARIES)
+    message(STATUS ${TAGLIB_LIBRARIES})
 endif()
