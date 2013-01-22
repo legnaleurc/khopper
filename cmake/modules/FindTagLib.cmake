e4426bbcf249c7253278489dd73c9c69507abe14
Wei Cheng Pan <legnaleurc@gmail.com>
2013-01-22 16:21:14 +0800

    Fix FindTagLib script.

--- cmake/modules/FindTagLib.cmake
+++ cmake/modules/FindTagLib.cmake
@@ -9,37 +9,40 @@
 
 include(KhopperUtility)
 
-if(NOT TAGLIB_FOUND)
-    find_package(PkgConfig)
-    if(PKG_CONFIG_FOUND)
-        pkg_check_modules(PC_TAGLIB taglib)
-        if(PC_TAGLIB_FOUND)
-            set(TAGLIB_INCLUDE_DIRS ${PC_TAGLIB_INCLUDE_DIRS})
-            set(TAGLIB_LIBRARIES ${PC_TAGLIB_LIBRARIES})
-        endif()
-    else()
-        path_from_env(TAGLIB_ROOT)
-
-        find_path(TAGLIB_INCLUDE_DIRS id3v2tag.h
-            HINTS ${TAGLIB_ROOT}
-            PATH_SUFFIXES "include/taglib")
-        find_library(TAGLIB_LIBRARIES_RELEASE tag
-            HINTS ${TAGLIB_ROOT}
-            PATH_SUFFIXES "lib")
-        find_library(TAGLIB_LIBRARIES_DEBUG tagd
-            HINTS ${TAGLIB_ROOT}
-            PATH_SUFFIXES "lib")
-
-        if(TAGLIB_LIBRARIES_RELEASE AND TAGLIB_LIBRARIES_DEBUG)
-            set(TAGLIB_LIBRARIES optimized ${TAGLIB_LIBRARIES_RELEASE} debug ${TAGLIB_LIBRARIES_DEBUG})
-        elseif(TAGLIB_LIBRARIES_RELEASE)
-            set(TAGLIB_LIBRARIES ${TAGLIB_LIBRARIES_RELEASE})
-        endif()
+find_package(PkgConfig QUIET)
+if(PKG_CONFIG_FOUND)
+    pkg_check_modules(PC_TAGLIB taglib)
+    if(PC_TAGLIB_FOUND)
+        set(TAGLIB_INCLUDE_DIRS ${PC_TAGLIB_INCLUDE_DIRS})
+        set(TAGLIB_LIBRARIES ${PC_TAGLIB_LIBRARIES})
     endif()
-
-    include(FindPackageHandleStandardArgs)
-
-    find_package_handle_standard_args(TAGLIB DEFAULT_MSG TAGLIB_INCLUDE_DIRS TAGLIB_LIBRARIES)
-
-    mark_as_advanced(TAGLIB_INCLUDE_DIRS TAGLIB_LIBRARIES)
 endif()
+
+path_from_env(TAGLIB_ROOT)
+
+if(NOT TAGLIB_INCLUDE_DIRS)
+    find_path(TAGLIB_INCLUDE_DIRS id3v2tag.h
+        HINTS ${TAGLIB_ROOT}
+        PATH_SUFFIXES "include/taglib")
+endif()
+
+if(NOT TAGLIB_LIBRARIES)
+    find_library(TAGLIB_LIBRARIES_RELEASE tag
+        HINTS ${TAGLIB_ROOT}
+        PATH_SUFFIXES "lib")
+    find_library(TAGLIB_LIBRARIES_DEBUG tagd
+        HINTS ${TAGLIB_ROOT}
+        PATH_SUFFIXES "lib")
+
+    if(TAGLIB_LIBRARIES_RELEASE AND TAGLIB_LIBRARIES_DEBUG)
+        set(TAGLIB_LIBRARIES optimized ${TAGLIB_LIBRARIES_RELEASE} debug ${TAGLIB_LIBRARIES_DEBUG})
+    elseif(TAGLIB_LIBRARIES_RELEASE)
+        set(TAGLIB_LIBRARIES ${TAGLIB_LIBRARIES_RELEASE})
+    endif()
+endif()
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(TAGLIB DEFAULT_MSG TAGLIB_INCLUDE_DIRS TAGLIB_LIBRARIES)
+
+mark_as_advanced(TAGLIB_INCLUDE_DIRS TAGLIB_LIBRARIES)
