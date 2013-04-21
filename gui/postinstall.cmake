f884277751e451e8b3ea79efec9fe15bda8a8bca
Wei Cheng Pan <legnaleurc@gmail.com>
2013-04-22 00:36:01 +0800

    Use $DESTDIR for package use.

--- gui/postinstall.cmake
+++ gui/postinstall.cmake
@@ -1,4 +1,4 @@
-file(READ ${CMAKE_INSTALL_PREFIX}/share/applications/khopper.desktop desktop)
+file(READ $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/share/applications/khopper.desktop desktop)
 string(REPLACE ";" "\;" desktop "${desktop}")
 string(REPLACE "__ICON__" "${CMAKE_INSTALL_PREFIX}/share/pixmap/khopper.png" desktop "${desktop}")
-file(WRITE ${CMAKE_INSTALL_PREFIX}/share/applications/khopper.desktop ${desktop})
+file(WRITE $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/share/applications/khopper.desktop ${desktop})
