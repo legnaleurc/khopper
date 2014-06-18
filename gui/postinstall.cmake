aad1bd77247daa0a49c9506f7dcdbe739e62a23d
Bruce Liu <brli@users.sourceforge.net>
2014-06-18 08:59:03 +0800

    edit gui/postinstall.cmake

--- gui/postinstall.cmake
+++ gui/postinstall.cmake
@@ -1,4 +1,4 @@
 file(READ $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/share/applications/khopper.desktop desktop)
 string(REPLACE ";" "\;" desktop "${desktop}")
-string(REPLACE "__ICON__" "${CMAKE_INSTALL_PREFIX}/share/pixmap/khopper.png" desktop "${desktop}")
+string(REPLACE "__ICON__" "${CMAKE_INSTALL_PREFIX}/share/pixmaps/khopper.png" desktop "${desktop}")
 file(WRITE $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/share/applications/khopper.desktop ${desktop})
